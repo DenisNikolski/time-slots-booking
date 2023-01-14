01912c7a2290d191b2ca68ac110d455cbbcf2e7f
Denis Nikolski <dzianisnikolski@gmail.com>
2023-01-14 23:41:08 +0100

    add display and create time slot in front end

--- front-end/time-slots-booking-react/src/App.js
+++ front-end/time-slots-booking-react/src/App.js
@@ -1,14 +1,25 @@
 import { Fragment, useState } from "react";
 import axios from "axios";
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 
-import { Button, DatePicker, message, Select, Form } from "antd";
+import {
+  Button,
+  DatePicker,
+  message,
+  Select,
+  Form,
+  Typography,
+  Radio,
+} from "antd";
 
 import "antd/dist/reset.css";
 import "./App.css";
 
 const { Option } = Select;
+const { Title } = Typography;
 const dateFormat = "YYYY-MM-DD";
+const api_base_url = process.env.REACT_APP_API_BASE_URL;
 
 const generateDurationOptions = () => {
   let optionsArray = [];
@@ -20,8 +31,9 @@ const generateDurationOptions = () => {
 const possibleDurationOptions = generateDurationOptions();
 
 const App = () => {
-  const [availableTimeSlots, setAvailableTimeSlots] = useState([]);
+  const [availableTimeSlots, setAvailableTimeSlots] = useState();
   const [messageApi, contextHolder] = message.useMessage();
+  dayjs.extend(utc);
 
   const handleDatePickerChange = (value) => {
     messageApi.info(
@@ -35,38 +47,68 @@ const App = () => {
     );
   };
 
-  const onFormSubmit = (values) => {
+  const onSearchFormSubmit = (values) => {
     console.log("Success:", values);
     getAvailableTimeSlots(values);
   };
 
+  const onSelectFormSubmit = (values) => {
+    console.log("Success:", values);
+    createBooking(values);
+  };
+
+  const formatDateTimeToHours = (dateTime) => dayjs(dateTime).format("HH:mm");
+
+  const displayApiErrors = (errors) => {
+    Object.keys(errors).forEach((key) => {
+      messageApi.error(`${key}: ${errors[key]}`);
+    });
+  };
+
   const getAvailableTimeSlots = async ({ date, duration }) => {
     try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API_BASE_URL}/available_time_slots`,
-        {
-          params: {
-            date: date.format(dateFormat),
-            booking_duration: duration,
-          },
-        }
-      );
-
+      const response = await axios.get(`${api_base_url}/available_time_slots`, {
+        params: {
+          date: dayjs.utc(date).format(dateFormat),
+          booking_duration: duration,
+        },
+      });
       const timeSlots = response.data;
       console.log(timeSlots);
       setAvailableTimeSlots(timeSlots);
     } catch ({ response }) {
       const { errors } = response.data;
-      Object.keys(errors).forEach((key) => {
-        messageApi.error(`${key}: ${errors[key]}`);
+      displayApiErrors(errors);
+      console.log(errors);
+    }
+  };
+
+  const createBooking = async ({ timeSlot }) => {
+    try {
+      const response = await axios.post(`${api_base_url}/booked_time_slots`, {
+        start: timeSlot.start,
+        end: timeSlot.end,
       });
+      const responseTimeSlot = response.data;
+      console.log(responseTimeSlot);
+      messageApi.success(
+        ` Booked time slot: ${formatDateTimeToHours(
+          responseTimeSlot.start
+        )}-${formatDateTimeToHours(responseTimeSlot.end)}`
+      );
+      setAvailableTimeSlots(null);
+    } catch ({ response }) {
+      const { errors } = response.data;
+      displayApiErrors(errors);
       console.log(errors);
     }
   };
 
   return (
-    <Fragment className="App">
-      <h1>Select desired date and duration</h1>
+    <div className="App">
+      <Typography>
+        <Title level={2}>Select desired date and duration</Title>
+      </Typography>
       {contextHolder}
       <Form
         labelCol={{
@@ -79,7 +121,7 @@ const App = () => {
           date: dayjs(),
           duration: 15,
         }}
-        onFinish={onFormSubmit}
+        onFinish={onSearchFormSubmit}
       >
         <Form.Item label="Date:" name="date">
           <DatePicker onChange={handleDatePickerChange} format={dateFormat} />
@@ -104,7 +146,53 @@ const App = () => {
           </Button>
         </Form.Item>
       </Form>
-    </Fragment>
+
+      {availableTimeSlots && (
+        <Fragment>
+          <Title level={2}>Select Time Slot</Title>
+          <Form
+            labelCol={{
+              span: 2,
+            }}
+            wrapperCol={{
+              span: 10,
+            }}
+            initialValues={{
+              date: dayjs(),
+              duration: 15,
+            }}
+            onFinish={onSelectFormSubmit}
+          >
+            <Form.Item
+              wrapperCol={{
+                offset: 2,
+                span: 14,
+              }}
+            >
+              <Form.Item
+                label="Available time slots:"
+                name="timeSlot"
+                rules={[
+                  { required: true, message: "Please select time slot!" },
+                ]}
+              >
+                <Radio.Group>
+                  {availableTimeSlots.map((timeSlot, index) => (
+                    <Radio key={index} value={timeSlot}>
+                      {formatDateTimeToHours(timeSlot.start)}-
+                      {formatDateTimeToHours(timeSlot.end)}
+                    </Radio>
+                  ))}
+                </Radio.Group>
+              </Form.Item>
+              <Button type="primary" htmlType="submit">
+                Confirm Booking
+              </Button>
+            </Form.Item>
+          </Form>
+        </Fragment>
+      )}
+    </div>
   );
 };
 
