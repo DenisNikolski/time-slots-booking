3403617f52d57794f554d395edb2a5ef76cf4b61
Denis Nikolski <dzianisnikolski@gmail.com>
2023-01-14 23:41:08 +0100

    add search form

--- front-end/time-slots-booking-react/src/App.js
+++ front-end/time-slots-booking-react/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Button, DatePicker, message, Select, Form } from "antd";
 
-function App() {
+import dayjs from "dayjs";
+
+import "antd/dist/reset.css";
+
+import "./App.css";
+
+const { Option } = Select;
+const dateFormat = "YYYY-MM-DD";
+
+const generateDurationOptions = () => {
+  let optionsArray = [];
+  for (let i = 1; i <= 24; i++) {
+    optionsArray.push(15 * i);
+  }
+  return optionsArray;
+};
+const possibleDurationOptions = generateDurationOptions();
+
+const App = () => {
+  const handleDatePickerChange = (value) => {
+    message.info(`Selected Date: ${value ? value.format(dateFormat) : "None"}`);
+  };
+
+  const handleDurationSelectChange = (value) => {
+    message.info(`Selected Duration: ${value ? `${value} minutes` : "None"}`);
+  };
+
+  const onFormSubmit = (values) => {
+    console.log("Success:", values);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Select desired date and duration</h1>
+      <Form
+        labelCol={{
+          span: 2,
+        }}
+        wrapperCol={{
+          span: 10,
+        }}
+        initialValues={{
+          date: dayjs(),
+          duration: 15,
+        }}
+        onFinish={onFormSubmit}
+      >
+        <Form.Item label="Date:" name="date">
+          <DatePicker onChange={handleDatePickerChange} format={dateFormat} />
+        </Form.Item>
+        <Form.Item label="Duration" name="duration">
+          <Select onChange={handleDurationSelectChange}>
+            {possibleDurationOptions.map((duration) => (
+              <Option key={duration} value={duration}>
+                {duration} minutes
+              </Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{
+            offset: 2,
+            span: 16,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Button type="primary" htmlType="submit">
+            Search
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
-}
+};
 
 export default App;
